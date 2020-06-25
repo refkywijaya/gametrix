9b70e099e58ad4aeddfc40fcc63f2c5881d2df1c
Refky Wijaya <priv8refky@gmail.com>
2020-06-25 22:07:20 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$url_claim = "https://gametrix-server.appspot.com/credit?user_guid=557701ac-f433-4a0e-9225-2f2a3d01a38f&type=video";
-$url_balance = "https://gametrix-server.appspot.com/get_user_balance?user_guid=557701ac-f433-4a0e-9225-2f2a3d01a38f&hasclicked=false&gaid=7d2dc3af-4ed1-49d1-8c6c-97212d12bb22&version_code=249&version_name=4.0.23&package_name=com.gametrix.rewards&user_language=en&v=3";
+$url_claim = "xxx";
+$url_balance = "xxx";
 
 ?>
